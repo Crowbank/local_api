2f28af27eb1848c6f837b007082ca485d81b7990
Eran Yehudai <crowbank.partners@gmail.com>
2018-03-22 23:52:33 +0000

    Testing OpenConnection

--- top.php
+++ top.php
@@ -1,2 +1,21 @@
 <?php 
 echo 'Welcome to the Crowbank Local Listener';
+
+function OpenConnection()
+{
+	try
+	{
+		$serverName = "tcp:myserver.database.windows.net,1433";
+		$connectionOptions = array("Database"=>"AdventureWorks",
+				"Uid"=>"MyUser", "PWD"=>"MyPassword");
+		$conn = sqlsrv_connect($serverName, $connectionOptions);
+		if($conn == false)
+			die(FormatErrors(sqlsrv_errors()));
+	}
+	catch(Exception $e)
+	{
+		echo("Error!");
+	}
+}
+
+OpenConnection();
